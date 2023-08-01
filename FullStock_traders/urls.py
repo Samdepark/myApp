fd09db2583874a891bed3c5caf7d2f1024a68c8a
samdepark <samdepark20@gmail.com>
2023-08-01 17:42:12 +0300

    rectification on the urls

--- FullStock_traders/urls.py
+++ FullStock_traders/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include;
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
-    path('shopping_cart/', include('Shopping_Cart.urls')),
+    path('products/', include('Products.url')),
+    path('shopping_cart/', include('Shopping_Cart.url')),
     path('ratings/', include('Ratings.url')),
 ]
--- Products/url.py
+++ Products/url.py
@@ -2,5 +2,5 @@ from  django.urls import path;
 from . import views;
 
 urlpatterns = [ 
-    path("products/", views.index, name = 'products' )
+    path("products/", views.index, name = 'Products' )
 ]
