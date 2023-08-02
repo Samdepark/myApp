a12e530f8a51fd10420f3345c508b3b6a2721c03
samdepark <samdepark20@gmail.com>
2023-08-02 18:28:09 +0300

    Auth config

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Authentication.models import user
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display =['username', 'email',]
+    
+
+admin.site.register(user)
