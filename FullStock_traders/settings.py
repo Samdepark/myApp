267ef15658edeb79ecf25284b5958351708272b9
rezarex <marvin.wekesa@gmail.com>
2023-09-11 14:43:21 +0300

    FEATURE: added swagger documentation

--- FullStock_traders/settings.py
+++ FullStock_traders/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-
+    'drf_spectacular',
     "Authentication",
 ]
 
@@ -76,7 +76,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS":"drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "FullStock Traders",
 }
 
 WSGI_APPLICATION = 'FullStock_traders.wsgi.application'
--- FullStock_traders/urls.py
+++ FullStock_traders/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin;
 from django.urls import path,include;
 from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 app_name = 'FullStock_traders'
@@ -9,4 +10,6 @@ app_name = 'FullStock_traders'
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/', include('Authentication.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/docs/',SpectacularSwaggerView.as_view(url_name="schema") )
 ]
